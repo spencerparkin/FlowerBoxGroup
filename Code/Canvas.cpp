2034a87cd233eb648e56aac6f4fcaf4521504321
SpencerParkin <spencerparkin@outlook.com>
2016-01-09 12:58:59 -0700

    box orientation

--- Code/Canvas.cpp
+++ Code/Canvas.cpp
@@ -7,11 +7,37 @@ int Canvas::attributeList[] = { WX_GL_RGBA, WX_GL_DOUBLEBUFFER, 0 };
 
 Canvas::Canvas( wxWindow* parent ) : wxGLCanvas( parent, wxID_ANY, attributeList )
 {
+	rotatingBoxWithMouse = false;
+
 	context = nullptr;
 	flowerBox = new FlowerBox();
 
+	orientMatrix[0] = 1.0;
+	orientMatrix[1] = 0.0;
+	orientMatrix[2] = 0.0;
+	orientMatrix[3] = 0.0;
+
+	orientMatrix[4] = 0.0;
+	orientMatrix[5] = 1.0;
+	orientMatrix[6] = 0.0;
+	orientMatrix[7] = 0.0;
+
+	orientMatrix[8] = 0.0;
+	orientMatrix[9] = 0.0;
+	orientMatrix[10] = 1.0;
+	orientMatrix[11] = 0.0;
+
+	orientMatrix[12] = 0.0;
+	orientMatrix[13] = 0.0;
+	orientMatrix[14] = 0.0;
+	orientMatrix[15] = 1.0;
+
 	Bind( wxEVT_PAINT, &Canvas::OnPaint, this );
 	Bind( wxEVT_SIZE, &Canvas::OnSize, this );
+	Bind( wxEVT_LEFT_DOWN, &Canvas::OnMouseLeftDown, this );
+	Bind( wxEVT_LEFT_UP, &Canvas::OnMouseLeftUp, this );
+	Bind( wxEVT_MOTION, &Canvas::OnMouseMotion, this );
+	Bind( wxEVT_MOUSE_CAPTURE_LOST, &Canvas::OnMouseCaptureLost, this );
 }
 
 /*virtual*/ Canvas::~Canvas( void )
@@ -40,11 +66,68 @@ void Canvas::OnPaint( wxPaintEvent& event )
 	glLoadIdentity();
 	gluLookAt( 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 );
 
+	glPushMatrix();
+	glMultMatrixf( orientMatrix );
+
+	DrawAxes();
+
 	flowerBox->Draw();
+
+	glPopMatrix();
 
 	glFlush();
 
 	SwapBuffers();
+}
+
+void Canvas::DrawAxes( void )
+{
+	glLineWidth( 2.f );
+	glBegin( GL_LINES );
+
+	glColor3f( 1.f, 0.f, 0.f );
+	glVertex3f( 0.f, 0.f, 0.f );
+	glVertex3f( 2.f, 0.f, 0.f );
+
+	glColor3f( 0.f, 1.f, 0.f );
+	glVertex3f( 0.f, 0.f, 0.f );
+	glVertex3f( 0.f, 2.f, 0.f );
+
+	glColor3f( 0.f, 0.f, 1.f );
+	glVertex3f( 0.f, 0.f, 0.f );
+	glVertex3f( 0.f, 0.f, 2.f );
+
+	glEnd();
+}
+
+void Canvas::GetOrientAxes( c3ga::vectorE3GA& xAxis, c3ga::vectorE3GA& yAxis, c3ga::vectorE3GA& zAxis )
+{
+	xAxis.m_e1 = orientMatrix[0];
+	xAxis.m_e2 = orientMatrix[1];
+	xAxis.m_e3 = orientMatrix[2];
+
+	yAxis.m_e1 = orientMatrix[4];
+	yAxis.m_e2 = orientMatrix[5];
+	yAxis.m_e3 = orientMatrix[6];
+
+	zAxis.m_e1 = orientMatrix[8];
+	zAxis.m_e2 = orientMatrix[9];
+	zAxis.m_e3 = orientMatrix[10];
+}
+
+void Canvas::SetOrientAxes( const c3ga::vectorE3GA& xAxis, const c3ga::vectorE3GA& yAxis, const c3ga::vectorE3GA& zAxis )
+{
+	orientMatrix[0] = xAxis.m_e1;
+	orientMatrix[1] = xAxis.m_e2;
+	orientMatrix[2] = xAxis.m_e3;
+
+	orientMatrix[4] = yAxis.m_e1;
+	orientMatrix[5] = yAxis.m_e2;
+	orientMatrix[6] = yAxis.m_e3;
+
+	orientMatrix[8] = zAxis.m_e1;
+	orientMatrix[9] = zAxis.m_e2;
+	orientMatrix[10] = zAxis.m_e3;
 }
 
 void Canvas::OnSize( wxSizeEvent& event )
@@ -55,6 +138,60 @@ void Canvas::OnSize( wxSizeEvent& event )
 	glViewport( 0, 0, size.GetWidth(), size.GetHeight() );
 
 	Refresh();
+}
+
+void Canvas::OnMouseLeftDown( wxMouseEvent& event )
+{
+	mousePos = event.GetPosition();
+	rotatingBoxWithMouse = true;
+	CaptureMouse();
+}
+
+void Canvas::OnMouseLeftUp( wxMouseEvent& event )
+{
+	ReleaseMouse();
+	rotatingBoxWithMouse = false;
+}
+
+void Canvas::OnMouseMotion( wxMouseEvent& event )
+{
+	if( rotatingBoxWithMouse )
+	{
+		wxPoint mouseDelta = event.GetPosition() - mousePos;
+		mousePos = event.GetPosition();
+
+		double mouseSensativityFactor = 0.3;
+
+		double xAxisMouseRotAngleDegs = double( mouseDelta.y ) * mouseSensativityFactor;
+		double yAxisMouseRotAngleDegs = double( mouseDelta.x ) * mouseSensativityFactor;
+
+		c3ga::vectorE3GA xAxisMouse( c3ga::vectorE3GA::coord_e1_e2_e3, 1.0, 0.0, 0.0 );
+		c3ga::vectorE3GA yAxisMouse( c3ga::vectorE3GA::coord_e1_e2_e3, 0.0, 1.0, 0.0 );
+
+		c3ga::rotorE3GA xAxisRotorMouse = FlowerBox::AxisAngleRotor( xAxisMouse, xAxisMouseRotAngleDegs );
+		c3ga::rotorE3GA yAxisRotorMouse = FlowerBox::AxisAngleRotor( yAxisMouse, yAxisMouseRotAngleDegs );
+
+		c3ga::vectorE3GA xAxis, yAxis, zAxis;
+		
+		GetOrientAxes( xAxis, yAxis, zAxis );
+
+		xAxis = c3ga::applyUnitVersor( xAxisRotorMouse, xAxis );
+		yAxis = c3ga::applyUnitVersor( xAxisRotorMouse, yAxis );
+		zAxis = c3ga::applyUnitVersor( xAxisRotorMouse, zAxis );
+
+		xAxis = c3ga::applyUnitVersor( yAxisRotorMouse, xAxis );
+		yAxis = c3ga::applyUnitVersor( yAxisRotorMouse, yAxis );
+		zAxis = c3ga::applyUnitVersor( yAxisRotorMouse, zAxis );
+
+		SetOrientAxes( xAxis, yAxis, zAxis );
+
+		Refresh();
+	}
+}
+
+void Canvas::OnMouseCaptureLost( wxMouseCaptureLostEvent& event )
+{
+	rotatingBoxWithMouse = false;
 }
 
 void Canvas::BindContext( void )
--- Code/Canvas.h
+++ Code/Canvas.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "FlowerBox.h"
+#include "c3ga/c3ga.h"
 #include <wx/glcanvas.h>
 
 class Canvas : public wxGLCanvas
@@ -14,12 +15,23 @@ public:
 
 	void OnPaint( wxPaintEvent& event );
 	void OnSize( wxSizeEvent& event );
+	void OnMouseLeftDown( wxMouseEvent& event );
+	void OnMouseLeftUp( wxMouseEvent& event );
+	void OnMouseMotion( wxMouseEvent& event );
+	void OnMouseCaptureLost( wxMouseCaptureLostEvent& event );
 
 	void BindContext( void );
+	void DrawAxes( void );
+
+	void GetOrientAxes( c3ga::vectorE3GA& xAxis, c3ga::vectorE3GA& yAxis, c3ga::vectorE3GA& zAxis );
+	void SetOrientAxes( const c3ga::vectorE3GA& xAxis, const c3ga::vectorE3GA& yAxis, const c3ga::vectorE3GA& zAxis );
 
 	static int attributeList[];
 	wxGLContext* context;
+	GLfloat orientMatrix[16];
 	FlowerBox* flowerBox;
+	bool rotatingBoxWithMouse;
+	wxPoint mousePos;
 };
 
 // Canvas.h
